73c7b8366e032ed56850d06ac1f682eeaf56330e
Leif <ac676175@gmail.com>
2019-04-05 00:26:07 +0800

    lesson 91

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () =>{
+    return (
+        <div style={{ textAlign: 'center'}}>
+            <h1>
+                Leif
+            </h1>
+            Collect
+        </div>
+    )
+}
+
+export default Landing;
