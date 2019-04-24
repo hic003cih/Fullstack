aa316369b6730eebd2ed265aff1ae7e8fd88c32c
Leif <ac676175@gmail.com>
2019-04-24 21:22:24 +0800

    finish kesson 165

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,11 +1,24 @@
 import React , { Component } from 'react';
-
+import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+    state = {showFormReview: false};
+    renderContent(){
+        if(this.state.showFormReview){
+            return <SurveyFormReview />
+        }
+
+        return (
+            <SurveyForm 
+                onSurveySubmit = { () => this.setState({ showFormReview: true })}
+            />
+        );
+    }
 render(){
    return(
     <div>
-        SurveyNew!
+        {this.renderContent()}
     </div>
    );
 }
